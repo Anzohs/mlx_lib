3bdaddf018df2c65dcc193be1205ae651c1540f8
hladeiro <10549215+Anzohs@users.noreply.github.com>
2024-10-29 14:39:40 +0000

    adding exit functions
    /

--- game/update_game.c
+++ game/update_game.c
@@ -17,11 +17,62 @@ static void	game_to_win(t_game *g)
 	mlx_put_image_to_window(g->mlx, g->win.win, g->cam.img, 0, 0);
 }
 
+static void	pixel_e(t_game *g, int src_x, int src_y)
+{
+	int				x;
+	int				y;
+	unsigned int	color;
+	t_img			*img;
+
+	y = -1;
+	while (++y < img->pos.y)
+	{
+		x = -1;
+		while (++x < img->pos.x)
+		{
+			color = *(unsigned int *)(img->addr + ((src_y + y) * \
+				img->line_len + (src_x + x) * (img->bpp / 8)));
+			if (color != 0xFF000000)
+				*(unsigned int *)(g->e.sprite.addr + (y * \
+					g->e.sprite.line_len + x * \
+					(g->c.sprite.bpp / 8))) = color;
+		}
+	}
+}
+
+static void	exit_to_w(t_img *w, t_img *e, t_vector pos)
+{
+		int				y;
+		int				x;
+		unsigned int	color;
+
+		y = -1;
+		while (++y < e->pos.y)
+		{
+			x = -1;
+			while (++x < e->pos.x)
+			{
+				color = *(unsigned int *)(e->addr + \
+					(y * e->line_len + x * (e->bpp / 8)));
+				if ((color & 0x00FFFFFF) != 0)
+					*(unsigned int *)(w->addr + ((pos.y + y) * \
+						w->line_len + (pos.x + x) * (w->bpp / 8))) = color;
+			}
+		}
+}
+
+static	void	draw_e(t_game *g)
+{
+	pixel_e(g, 0, 0);
+	exit_to_w(g->world.img, &g->e.sprite, g->e.pos);
+}
+
 static void	render_game(t_game *g)
 {
 	render_map(g, g->map);
 	render_coll(g);
 	draw_p(g, g->p.anim.frame * g->p.sprite.pos.x, g->p.anim.row * g->p.sprite.pos.y);
+	draw_e(g);
 	update_cam(g);
 	game_to_win(g);
 }
--- game_start.c
+++ game_start.c
@@ -28,6 +28,10 @@ static void	init_exit(t_game *g)
 	g->e.sprite = (t_img){0};
 	g->e.sprite.img = mlx_xpm_file_to_image(g->mlx, "exit.xpm", &g->e.sprite.pos.x, &g->e.sprite.pos.y);
 	g->e.sprite.addr = mlx_get_data_addr(g->e.sprite.img, &g->e.sprite.bpp, &g->e.sprite.line_len, &g->e.sprite.endian);
+	g->e.pos.x *= 32;
+	g->e.pos.y *= 32;
+	g->e.sprite.pos = (t_vector){32, 32};
+	g->e.is_open = false;
 }
 
 static int	game_loop(t_game *g)
@@ -49,6 +53,7 @@ void	game_start(t_map map)
 	init_cam(g, (t_vector){800, 600});
 	write_world(g, map);
 	player_start(g);
+	init_exit(g);
 	start_collect(g, map.c);
 	event_handler(g);
 	g->loop(g);
